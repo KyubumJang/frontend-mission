00236405458dea2c229b74e1005d48413d5ab119
Kyubum Jang <jkb2221@gmail.com>
2022-01-09 21:31:18 +0900

    feat: HomeMission Component 구현

---
+++ mission/src/components/HomeMission.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home-input">
+    <input v-model="message" placeholder="">
+    <button v-on:click='rotateMessage'>제출</button>
+    <button v-on:click='NotificationEvent'>알림</button>
+    <p>{{ message }}</p>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'HomeMission',
+  data() {
+    return {
+      message: this.message,
+      counter: 0,
+    };
+  },
+  methods: {
+    rotateMessage() {
+      this.message = this.message.slice(1) + this.message[0];
+    },
+    NotificationEvent() {
+      this.counter += 1;
+
+      alert(`${this.message} ${this.counter}`);
+    },
+  },
+
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+p {
+  color: #42b983;
+}
+</style>
