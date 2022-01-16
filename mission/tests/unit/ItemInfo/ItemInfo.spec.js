06e5e6dd3ce601cb51a22779c327da6d6c63baa6
Kyubum Jang <jkb2221@gmail.com>
2022-01-16 21:55:20 +0900

    test: ItemInfo에 대해 테스트 코드 작성 시도

--- mission/tests/unit/ItemInfo/ItemInfo.spec.js
+++ mission/tests/unit/ItemInfo/ItemInfo.spec.js
@@ -2,9 +2,16 @@ import { mount } from '@vue/test-utils';
 import ItemInfoPage from '@/views/ItemInfo.vue';
 
 describe('ItemInfoPage', () => {
-  it('redners ItemInfoPage', () => {
-    const wrapper = mount(ItemInfoPage);
-
+  const wrapper = mount(ItemInfoPage);
+  it('renders ItemInfoPage', () => {
     expect(wrapper.find('#item-info-page').exists()).toBe(true);
+  });
+
+  it('renders productImage', () => {
+    expect(wrapper.find('product=image').exists()).toBe(true);
+  });
+
+  it('renders productImage', () => {
+    expect(wrapper.find('product=image').exists()).toBe(true);
   });
 });
