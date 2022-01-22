2dfeef9d985e45a3c807a1a5dc9d12cba7e23abc
Kyubum Jang <jkb2221@gmail.com>
2022-01-23 04:42:33 +0900

    feat: add Header Component

---
+++ mission/src/components/ItemList/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="header">
+    <h2 class="header-title" data-test="header-title">{{ headerTitle }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: ['headerTitle'],
+};
+</script>
+
+<style>
+.header {
+  position: absolute;
+  width: 390px;
+  height: 70px;
+  left: 0px;
+  top: 0px;
+}
+</style>
