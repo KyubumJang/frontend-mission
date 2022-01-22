e78511acb88c28e431dfe51a96910d86cd169c3f
Kyubum Jang <jkb2221@gmail.com>
2022-01-23 03:42:55 +0900

    test: add header test code

--- mission/tests/unit/ItemList/ItemList.spec.js
+++ mission/tests/unit/ItemList/ItemList.spec.js
@@ -1,10 +1,28 @@
 import { mount } from '@vue/test-utils';
-import ItemInfoPage from '@/views/ItemList.vue';
+import ItemListPage from '@/views/ItemList.vue';
 
 describe('ItemListPage', () => {
-  it('redners ItemListPage', () => {
-    const wrapper = mount(ItemInfoPage);
-
+  it('renders ItemListPage', () => {
+    const wrapper = mount(ItemListPage);
     expect(wrapper.find('#item-list-page').exists()).toBe(true);
+  });
+
+  // 헤더 컴포넌트
+  it('renders Header Component', () => {
+    const wrapper = mount(ItemListPage);
+    expect(wrapper.find('#header').exists()).toBe(true);
+  });
+
+  it('ItemListPage headerTitle equal Header Component headerTitle', () => {
+    const testHeaderTitle = 'My Shopping Mall';
+
+    const wrapper = mount(ItemListPage, {
+      data() {
+        return {
+          headerTitle: testHeaderTitle,
+        };
+      },
+    });
+    expect(wrapper.find('[data-test="header-title"').text()).toBe(testHeaderTitle);
   });
 });
