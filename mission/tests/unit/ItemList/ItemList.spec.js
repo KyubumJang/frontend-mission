1cdb2e9900044a54e5c56d2ffe5feb22e4453777
Kyubum Jang <jkb2221@gmail.com>
2022-01-23 04:40:48 +0900

    test: add Navigation test

--- mission/tests/unit/ItemList/ItemList.spec.js
+++ mission/tests/unit/ItemList/ItemList.spec.js
@@ -25,4 +25,31 @@ describe('ItemListPage', () => {
     });
     expect(wrapper.find('[data-test="header-title"').text()).toBe(testHeaderTitle);
   });
+
+  // NAV 컴포넌트
+  it('renders NavBar Component', () => {
+    const wrapper = mount(ItemListPage);
+    expect(wrapper.find('#nav').exists()).toBe(true);
+  });
+
+  it('renders home image, home text', () => {
+    const testHomeTitle = '홈';
+    const wrapper = mount(ItemListPage);
+    expect(wrapper.find('[data-test="home-title"]').text()).toBe(testHomeTitle);
+  });
+  it('renders home image, home text', () => {
+    const testLikeTitle = '찜';
+    const wrapper = mount(ItemListPage);
+    expect(wrapper.find('[data-test="like-title"]').text()).toBe(testLikeTitle);
+  });
+  it('renders home image, home text', () => {
+    const testCartTitle = '장바구니';
+    const wrapper = mount(ItemListPage);
+    expect(wrapper.find('[data-test="cart-title"]').text()).toBe(testCartTitle);
+  });
+  it('renders home image, home text', () => {
+    const testMyPageTitle = '마이 페이지';
+    const wrapper = mount(ItemListPage);
+    expect(wrapper.find('[data-test="mypage-title"]').text()).toBe(testMyPageTitle);
+  });
 });
