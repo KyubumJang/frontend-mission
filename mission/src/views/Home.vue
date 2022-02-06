c448536ce5fcaac9033a451864e58b036f1b72ed
Kyubum Jang <jkb2221@gmail.com>
2022-02-06 21:52:37 +0900

    refactor: API 연동을 위해 파일 수정

--- mission/src/views/Home.vue
+++ mission/src/views/Home.vue
@@ -2,20 +2,17 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
-    <HomeMission />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
-import HomeMission from '@/components/HomeMission.vue';
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
-    HomeMission,
   },
 };
 </script>
--- mission/src/views/ItemList.vue
+++ mission/src/views/ItemList.vue
@@ -1,137 +1,77 @@
 <template>
-  <div class="item-list" id="item-list-page">
-    <Header id="header" :headerTitle="headerTitle" />
-    <!-- v-for로 해결할 수 있을 듯 하지만 일단 구현부터 하기로 판단하였습니다. -->
-    <!-- 이 아래 class="two-col" 아이템 리스트를 컴포넌트로 만들 수 있을 듯 한데 방법을 모르겠습니다. -->
-    <div class="item-wrapper" id="item-wrapper">
-      <div class="two-col">
-        <Item
-          :price="item[0].price"
-          :original_price="item[0].original_price"
-          :name="item[0].name"
-          :description="item[0].description"
-        />
-        <Item
-          :price="item[1].price"
-          :original_price="item[1].original_price"
-          :name="item[1].name"
-          :description="item[1].description"
-        />
+  <Header class="item-list-header" />
+    <main>
+      <div class="w3-row item-list-main" data-test="item-list-page">
+        <ItemListItem
+          v-for="item in items"
+          :name="item.name"
+          :description="item.description"
+          :price="item.price"
+          :original_price="item.original_price"
+          :key="item.name"
+          :product_no="item.product_no"
+          class="w3-col s6"
+          data-test="item"
+          >
+        </ItemListItem>
       </div>
-      <div class="two-col">
-        <Item
-          :price="item[0].price"
-          :original_price="item[0].original_price"
-          :name="item[0].name"
-          :description="item[0].description"
-        />
-        <Item
-          :price="item[1].price"
-          :original_price="item[1].original_price"
-          :name="item[1].name"
-          :description="item[1].description"
-        />
-      </div>
-      <div class="two-col">
-        <Item
-          :price="item[0].price"
-          :original_price="item[0].original_price"
-          :name="item[0].name"
-          :description="item[0].description"
-        />
-        <Item
-          :price="item[1].price"
-          :original_price="item[1].original_price"
-          :name="item[1].name"
-          :description="item[1].description"
-        />
-      </div>
-      <div class="two-col">
-        <Item
-          :price="item[0].price"
-          :original_price="item[0].original_price"
-          :name="item[0].name"
-          :description="item[0].description"
-        />
-        <Item
-          :price="item[1].price"
-          :original_price="item[1].original_price"
-          :name="item[1].name"
-          :description="item[1].description"
-        />
-      </div>
-      <div class="two-col">
-        <Item
-          :price="item[0].price"
-          :original_price="item[0].original_price"
-          :name="item[0].name"
-          :description="item[0].description"
-        />
-        <Item
-          :price="item[1].price"
-          :original_price="item[1].original_price"
-          :name="item[1].name"
-          :description="item[1].description"
-        />
-      </div>
-    </div>
-    <Nav id="nav" />
-  </div>
+    </main>
+    <ItemListNav />
 </template>
 
 <script>
 import Header from '@/components/ItemList/Header.vue';
-import Item from '@/components/ItemList/Item.vue';
-import Nav from '@/components/ItemList/NavBar.vue';
+import ItemListItem from '@/components/ItemList/Item.vue';
+import ItemListNav from '@/components/ItemList/Nav.vue';
+import {
+  getItem,
+} from '../api/Item';
 
 export default {
   name: 'ItemListPage',
   components: {
     Header,
-    Item,
-    Nav,
+    ItemListItem,
+    ItemListNav,
   },
   data() {
     return {
-      headerTitle: 'My Shopping Mall',
-      item: [
-        {
-          item_no: 1,
-          item_image: '',
-          price: 20000,
-          original_price: 40000,
-          name: 'product 1',
-          description: 'description 1',
-        },
-        {
-          item_no: 2,
-          item_image: '',
-          price: 40000,
-          original_price: 40000,
-          name: 'product 2',
-          description: 'description 2',
-        },
-      ],
+      loading: false,
+      items: null,
+      error: null,
     };
   },
-  methods: {},
+  methods: {
+    async fetchData() {
+      this.error = null;
+      this.items = null;
+      this.loading = true;
+      try {
+        const res = await getItem();
+        this.loading = false;
+        this.items = res.data.items;
+      } catch (err) {
+        this.error = err.toString();
+      }
+    },
+  },
   computed: {},
+  created() {
+    this.fetchData();
+  },
 };
 </script>
 
 <style>
+.item-list-main {
+  padding-bottom: 60px;
+}
+
 .item-list {
   position: relative;
   overflow-y: scroll;
 }
-.item-wrapper {
-  padding-top: 70px;
-  padding-bottom: 70px;
-}
-.two-col {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-around;
+.item-list-header {
+  margin-bottom: 50px
 }
 </style>
