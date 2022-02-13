53bf9317ed431d75e736bb25505169cdea19830e
Kyubum Jang <jkb2221@gmail.com>
2022-02-13 18:29:41 +0900

    feat: 주문완료페이지 추가

---
+++ mission/src/views/OrderCompletePage.vue
@@ -0,0 +1,39 @@
+<template>
+  <main>
+    <div class="w3-container w3-center w3-col order-complete-main" data-test="order-complete-page">
+        <div class="icon">
+        <i class="fas fa-2x fa-check"></i>
+        </div>
+        <h3>주문이 완료되었습니다.</h3>
+        <a href="http://localhost:8080/" class="w3-button w3-hover-gray w3-border">
+        홈으로
+        </a>
+
+    </div>
+  </main>
+  <ItemListNav />
+</template>
+
+<script>
+
+export default {
+  name: 'OrderCompletePage',
+  components: {
+  },
+  data() {},
+  methods: {},
+  computed: {},
+};
+</script>
+
+<style>
+.order-complete-main {
+    margin: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-right: -50%;
+    transform: translate(-50%, -50%);
+    padding-bottom: 60px;
+}
+</style>
